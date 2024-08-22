e3af633233f8ca7e45a23db1fb9d66567fce58d0
Arthur <mermet.arthur@gmail.com>
2024-08-22 18:31:46 +0200

    better usability

--- main.py
+++ main.py
@@ -9,124 +9,12 @@ ou de resoudre une grille existante.
 import tkinter as tk
 import SAT_solve as sat
 import instance_en_DIMACS as id
+import argparse
 global zone
 global compteur
 global fichier
 zone=[]
 compteur=0
-
-#couleurs interface:
-cases = '#15699F'
-bg = '#DDE6ED'
-bord = '#203046'
-
-#choix du mode:
-print("\033c")
-s= int(input("Entrez \n- 1 pour créer une grille\n- 2 Pour résoudre une grille existante (le fichier instance doit etre dans 'grilles')\n: "))
-if s==1:
-   n = int(input("Taille de la grille : "))
-   taille = 550 / n
-   fichier = open("grilles_made/grille"+str(n)+'.txt' , "w")
-   fichier.write(str(n)+"\n")
-
-elif s==2:
-   nom = input("Nom du fichier grille : ")
-   if nom.endswith(".txt"):
-      nom = nom[:-4]
-   n = int(input("Taille de la grille : "))
-   taille = 550 / n
-
-else:
-   print("Erreur de saisie")
-   exit()
-   
-   
-
-
-# Crée la fenêtre principale et le canevas pour la grille
-root = tk.Tk()
-root.title("Solveur NoriNori")
-root.geometry("700x700+{}+{}".format(root.winfo_screenwidth()//2-350, root.winfo_screenheight()//2-450))
-root.attributes('-topmost', 1)
-canvas = tk.Canvas(root, width=700, height=700, bg=bg)
-canvas.pack()
-
-
-#dict pour associer les coordonnées des cases sur la fenetre a leur coordonnées dans la grille:
-#(la premiere case est en (0,0) sur la grille mais l'objet est en (75,25) sur la fenetre graphique)
-d={}
-# Dessine la grille
-for i in range(n):
-    for j in range(n):
-         x1, y1 = j*taille+75 , i*taille+25
-         x2, y2 = x1+taille, y1+taille
-         d[(x1,y1)] = (i,j)       
-
-         case = canvas.create_rectangle(x1, y1, x2, y2, fill=bg, width=1, outline=bord )
-         #effet de grossissement de la case au survol:
-         canvas.tag_bind(case, '<Enter>', lambda event, case=case: canvas.itemconfigure(case, width=2))
-         canvas.tag_bind(case, '<Leave>', lambda event, case=case: canvas.itemconfigure(case, width=1))
-canvas.create_rectangle(75, 25, 625, 575, width=5, outline=bord)
-
-
-
-#-----------------mode resolution de grille existante-------------------
-if s==2:
-   #tracer le contours des zones:
-   dimension,zones = id.fichier_instance_en_zones("grilles/"+nom+".txt")
-   for i in range(dimension):
-      for j in range(dimension):
-         x1, y1 = j*taille+75, i*taille+25      
-         for k in range(len(zones)):
-            if (i,j) in zones[k]:
-               zone=k
-   
-         if (i-1,j) not in zones[zone]:
-            canvas.create_line(x1, y1, x1+taille, y1, width=5, fill=bord, state='disabled')
-         if (i,j-1) not in zones[zone]:
-            canvas.create_line(x1, y1, x1, y1+taille, width=5, fill=bord, state='disabled')
-            
-   #montre la solution:
-   solution = sat.solveur("grilles/"+nom)
-   for case in solution:
-      if case>0:
-         canvas.itemconfigure(case, fill=cases)
-   if solution == []:
-      for i in range (dimension**2):
-         canvas.itemconfigure(i+1, fill='#FFA2A2')
-
-
-
-
-
-
-#-----------------mode creation de grille-------------------
-else:
-   #bouton pour valider la zone:
-
-   canvas.bind('<B1-Motion>', lambda event: ajouter(event))
-   canvas.bind('<B3-Motion>', lambda event: retirer(event))
-   canvas.bind('<Button-1>', lambda event: ajouter(event))
-   canvas.bind('<Button-3>', lambda event: retirer(event))
-
-   bouton = tk.Button(root, text="Créér une zone" , width=15, height=3, bg="#2F4767", fg="#F1F6F9", relief="flat")
-   bouton.pack()
-   bouton.place(x=90, y=610)
-   bouton.bind('<Button-1>', lambda event: valider_zone())
-
-   #bouton pour valider la grille:
-   bouton2 = tk.Button(root, text="Afficher la solution", width=15, height=3, bg="#2F4767", fg="#F1F6F9", relief="flat")
-   bouton2.pack()
-   bouton2.place(x=292, y=610)
-   bouton2.bind('<Button-1>', lambda event: afficher_solution())
-
-   #bouton reset:
-   bouton3 = tk.Button(root, text="Recommencer", width=15, height=3, bg="#2F4767", fg="#F1F6F9", relief="flat")
-   bouton3.pack()
-   bouton3.place(x=500, y=610)
-   #bind pour appeler la fonction reset et reinitialiser le fichier:
-   bouton3.bind('<Button-1>', lambda event: reset())
-   
 
 
 #fonctions d'evenements interface
@@ -234,11 +122,80 @@ def reset():
 
 
 
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser(description='Solveur NoriNori')
+   parser.add_argument('grid_size', type=int, help='Taille de la grille')
+   
+   args = parser.parse_args()
+   
+   n = args.grid_size
 
-root.mainloop()
-if s==1:
-   fichier.close()
+   #couleurs interface:
+   cases = '#15699F'
+   bg = '#DDE6ED'
+   bord = '#203046'
 
+
+   taille = 550 / n
+   fichier = open("grilles_made/grille"+str(n)+'.txt' , "w")
+   fichier.write(str(n)+"\n")
+
+
+   # Crée la fenêtre principale et le canevas pour la grille
+   root = tk.Tk()
+   root.title("Solveur NoriNori")
+   root.geometry("700x700+{}+{}".format(root.winfo_screenwidth()//2-350, root.winfo_screenheight()//2-450))
+   root.attributes('-topmost', 1)
+   canvas = tk.Canvas(root, width=700, height=700, bg=bg)
+   canvas.pack()
+
+
+   #dict pour associer les coordonnées des cases sur la fenetre a leur coordonnées dans la grille:
+   #(la premiere case est en (0,0) sur la grille mais l'objet est en (75,25) sur la fenetre graphique)
+   d={}
+   # Dessine la grille
+   for i in range(n):
+      for j in range(n):
+            x1, y1 = j*taille+75 , i*taille+25
+            x2, y2 = x1+taille, y1+taille
+            d[(x1,y1)] = (i,j)       
+
+            case = canvas.create_rectangle(x1, y1, x2, y2, fill=bg, width=1, outline=bord )
+            #effet de grossissement de la case au survol:
+            canvas.tag_bind(case, '<Enter>', lambda event, case=case: canvas.itemconfigure(case, width=2))
+            canvas.tag_bind(case, '<Leave>', lambda event, case=case: canvas.itemconfigure(case, width=1))
+   canvas.create_rectangle(75, 25, 625, 575, width=5, outline=bord)
+
+
+
+
+   #bouton pour valider la zone:
+
+   canvas.bind('<B1-Motion>', lambda event: ajouter(event))
+   canvas.bind('<B3-Motion>', lambda event: retirer(event))
+   canvas.bind('<Button-1>', lambda event: ajouter(event))
+   canvas.bind('<Button-3>', lambda event: retirer(event))
+
+   bouton = tk.Button(root, text="Créér une zone" , width=15, height=3, bg="#2F4767", fg="#F1F6F9", relief="flat")
+   bouton.pack()
+   bouton.place(x=90, y=610)
+   bouton.bind('<Button-1>', lambda event: valider_zone())
+
+   #bouton pour valider la grille:
+   bouton2 = tk.Button(root, text="Afficher la solution", width=15, height=3, bg="#2F4767", fg="#F1F6F9", relief="flat")
+   bouton2.pack()
+   bouton2.place(x=292, y=610)
+   bouton2.bind('<Button-1>', lambda event: afficher_solution())
+
+   #bouton reset:
+   bouton3 = tk.Button(root, text="Recommencer", width=15, height=3, bg="#2F4767", fg="#F1F6F9", relief="flat")
+   bouton3.pack()
+   bouton3.place(x=500, y=610)
+   #bind pour appeler la fonction reset et reinitialiser le fichier:
+   bouton3.bind('<Button-1>', lambda event: reset())
+
+
+   root.mainloop()
 
 """
 bleu:
